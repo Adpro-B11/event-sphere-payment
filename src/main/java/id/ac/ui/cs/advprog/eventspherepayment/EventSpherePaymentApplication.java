afa4d80c0570d17cab8eb0f1ed50c2f66923e8d5
adlynfakhreyz <adlynuniacc@gmail.com>
2025-05-14 01:37:22 +0700

    feat: init

---
+++ src/main/java/id/ac/ui/cs/advprog/eventspherepayment/EventSpherePaymentApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.eventspherepayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSpherePaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventSpherePaymentApplication.class, args);
+    }
+
+}
