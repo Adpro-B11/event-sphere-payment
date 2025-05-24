b6803a506c9986dbb3dd212318ead6af4a326712
Simforianus.Jonathan <jonathanharefa9@gmail.com>
2025-05-25 05:43:07 +0700

    [FIX] Add configurations for running app and running tests

--- src/main/java/id/ac/ui/cs/advprog/eventspherepayment/EventSpherePaymentApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eventspherepayment/EventSpherePaymentApplication.java
@@ -2,12 +2,26 @@ package id.ac.ui.cs.advprog.eventspherepayment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class EventSpherePaymentApplication {
 
     public static void main(String[] args) {
+
+        try {
+            Dotenv dotenv = Dotenv.load();
+            System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
+            System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
+            System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
+            System.out.println("SUCCESS: .env file loaded successfully");
+        } catch (Exception e) {
+            System.out.println("ERROR: .env file not found");
+        }
+
         SpringApplication.run(EventSpherePaymentApplication.class, args);
     }
+
+
 
 }
