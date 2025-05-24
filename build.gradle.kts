8a61e675a48c38dd77706376b81e7eb2088dc904
Simforianus.Jonathan <jonathanharefa9@gmail.com>
2025-05-24 23:34:38 +0700

    [CHORE] add depedency for gradle and xml

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,11 +30,17 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     // Dependensi JPA
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    // Dotenv
+    implementation("io.github.cdimascio:dotenv-java:3.0.0")
 
     compileOnly("org.projectlombok:lombok")
 
