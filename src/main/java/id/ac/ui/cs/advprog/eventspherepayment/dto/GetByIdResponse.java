231a154cee97df1dd72223d4606b9b9853936958
Simforianus.Jonathan <jonathanharefa9@gmail.com>
2025-05-25 07:55:31 +0700

    [RED] create dto structure for response to front end

---
+++ src/main/java/id/ac/ui/cs/advprog/eventspherepayment/dto/GetByIdResponse.java
@@ -0,0 +1,15 @@
+package id.ac.ui.cs.advprog.eventspherepayment.dto;
+
+import id.ac.ui.cs.advprog.eventspherepayment.model.Transaction;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetByIdResponse {
+    private Transaction data;
+}
+
