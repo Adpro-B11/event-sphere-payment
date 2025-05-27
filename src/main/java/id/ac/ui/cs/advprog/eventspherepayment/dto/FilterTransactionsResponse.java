52bb9afcff37f5284b06acfe337a76a61a5eec3e
Simforianus.Jonathan <jonathanharefa9@gmail.com>
2025-05-27 08:27:06 +0700

    [REFACTOR] add dto

--- src/main/java/id/ac/ui/cs/advprog/eventspherepayment/dto/FilterTransactionsResponse.java
+++ src/main/java/id/ac/ui/cs/advprog/eventspherepayment/dto/FilterTransactionsResponse.java
@@ -1,6 +1,5 @@
 package id.ac.ui.cs.advprog.eventspherepayment.dto;
 
-import id.ac.ui.cs.advprog.eventspherepayment.model.Transaction;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +10,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class FilterTransactionsResponse {
-    private List<Transaction> data;
+    private List<TransactionResponse> data;
 }
+
 
--- src/main/java/id/ac/ui/cs/advprog/eventspherepayment/dto/PurchaseRequest.java
+++ src/main/java/id/ac/ui/cs/advprog/eventspherepayment/dto/PurchaseRequest.java
@@ -1,32 +1,18 @@
 package id.ac.ui.cs.advprog.eventspherepayment.dto;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import lombok.Builder;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Map;
+
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class PurchaseRequest {
-    @NotBlank            private String ticketId;
-    @NotBlank            private String eventId;
-    @Min(value = 0, message = "amount must be non-negative")
+    private String userId;
     private double amount;
-    @Min(value = 0, message = "quantity must be non-negative")
-    private int quantity;
-    @NotBlank            private String paymentMethod;
-
-    @Builder
-    public PurchaseRequest(String ticketId,
-                           String eventId,
-                           double amount,
-                           int quantity,
-                           String paymentMethod) {
-        this.ticketId     = ticketId;
-        this.eventId      = eventId;
-        this.amount       = amount;
-        this.quantity     = quantity;
-        this.paymentMethod= paymentMethod;
-    }
+    private String eventId;
+    private Map<String, String> ticketData;
 }
