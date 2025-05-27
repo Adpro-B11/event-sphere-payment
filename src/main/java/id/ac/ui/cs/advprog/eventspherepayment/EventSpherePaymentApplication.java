de90e4b7c223f05def211c73ee25823186b00d03
Ariqmau <ariqmalik@gmail.com>
2025-05-27 11:21:29 +0700

    [CI/CD] deploy setup

--- src/main/java/id/ac/ui/cs/advprog/eventspherepayment/EventSpherePaymentApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eventspherepayment/EventSpherePaymentApplication.java
@@ -16,7 +16,8 @@ public class EventSpherePaymentApplication {
             System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
             System.setProperty("jwt.secret-key", dotenv.get("JWT_SECRET_KEY"));
             System.setProperty("jwt.expiration", dotenv.get("JWT_EXPIRATION"));
-            System.setProperty("auth.service.url", dotenv.get("auth.service.url"));
+            System.setProperty("auth.service.base-url", dotenv.get("AUTH_SERVICE_BASE_URL"));
+            System.setProperty("event.service.base-url", dotenv.get("EVENT_SERVICE_BASE_URL"));
             System.out.println("SUCCESS: .env file loaded successfully");
             System.out.println("Using DB URL: " + System.getProperty("spring.datasource.url"));
             System.out.println("DB Username: " + System.getProperty("spring.datasource.username"));
