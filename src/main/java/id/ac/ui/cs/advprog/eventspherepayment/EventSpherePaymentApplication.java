25fafc56eeb4185804597632dcaba9873b2021a7
Simforianus.Jonathan <jonathanharefa9@gmail.com>
2025-05-27 08:22:46 +0700

    [FIX] fix depedencies and plugin

--- src/main/java/id/ac/ui/cs/advprog/eventspherepayment/EventSpherePaymentApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eventspherepayment/EventSpherePaymentApplication.java
@@ -11,12 +11,16 @@ public class EventSpherePaymentApplication {
 
         try {
             Dotenv dotenv = Dotenv.load();
-//            System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
-//            System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
-//            System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
+            System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
+            System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
+            System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
             System.setProperty("jwt.secret-key", dotenv.get("JWT_SECRET_KEY"));
             System.setProperty("jwt.expiration", dotenv.get("JWT_EXPIRATION"));
+            System.setProperty("auth.service.url", dotenv.get("auth.service.url"));
             System.out.println("SUCCESS: .env file loaded successfully");
+            System.out.println("Using DB URL: " + System.getProperty("spring.datasource.url"));
+            System.out.println("DB Username: " + System.getProperty("spring.datasource.username"));
+
         } catch (Exception e) {
             System.out.println("ERROR: .env file not found");
         }
